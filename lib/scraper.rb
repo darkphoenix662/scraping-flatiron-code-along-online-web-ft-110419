d452db086ffbb222b9e311af25e283822c719875
Sean Good <tree662@protonmail.com>
2019-12-06 18:56:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    html = open("https://flatironschool.com/")
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry 
   end
   
   def get_courses
@@ -32,4 +32,4 @@ class Scraper
 end
 
 
-
+Scraper.new.get_page
