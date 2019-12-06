d0d2985f8acef9bdb44c99f914b77e98e17e1477
Sean Good <tree662@protonmail.com>
2019-12-06 18:57:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,24 +10,24 @@ class Scraper
     binding.pry 
   end
   
-  def get_courses
+  # def get_courses
     
-  end
+  # end
   
-  def make_courses
+  # def make_courses
     
-  end
+  # end
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
   
 end
 
